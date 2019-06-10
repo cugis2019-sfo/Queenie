373a6c6e7af4d16884634bedb10376049353e6f2
Queenietran509 <51681292+Queenietran509@users.noreply.github.com>
2019-06-10 16:54:26 -0700

    Add files via upload

---
+++ hey.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 14:10:39 2019
+
+@author: admin
+"""
+
+5/8 #division
+print("hello ,5,World")
+print("hey")
+Print("5")
+5*2 #multiplied
+5/2 #division
+5-2 #subtraction 
+5**2 #raised the power to 
+8/9*3 #division and multiply
+
+def sum(a,b,c):
+    sum = a + b+ c 
+    print("The sum of",a,"and", b ,"and", c "is", sum )
+
+sum(4,5)
+
+def trianglearea(a,b):
+    trianglearea= a * b * 0.5
+    print ("the area of a triangle", trianglearea) 
+
+trianglearea (3,4)
