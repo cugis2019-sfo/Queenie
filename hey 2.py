b84b6e4aea737d65cb884d48902c416fc92d0710
Queenietran509 <51681292+Queenietran509@users.noreply.github.com>
2019-06-12 15:27:01 -0700

    Add files via upload

---
+++ hey 2.py
@@ -0,0 +1,75 @@
+cadbury1 = "milk chocolate"
+cadbury2 = "dark chocolate"
+cadbury3 = "white chocolate"
+
+cadurymilk = 5
+cadburydark = 3
+cadburywhite = 8
+
+chocolate1 = {"cadburymilk": 5}
+chocolate2 = {"cadburydark": 8}
+chocolate3 = {"cadburywhite":3}
+
+print(chocolate1, chocolate2, chocolate3)
+
+chocolcates = {"cadburymilk": 5, "cadburydark": 8, " cadburywhite": 3}
+print(chocolate)
+
+
+studentinfo("steve":32,"lia:28,"vin":45,"katie":38)
+print(studentinfo)
+
+Studentgender{"steve":"male","lia":"female", "vin":"male","katie":"female"}
+print(studentgender)
+
+print(studentgender)
+print(studentinfo)
+
+import pandas
+dir(pandas)
+
+studentinfo = pandas.Series(studentinfocal)
+print(studentinfocal)
+
+studentgender = pandas.Series(gender)
+print(student)
+
+chocolate = pandas.Series(chocolate)
+print(chocolate)
+
+#dataFrame]
+chocolatedata = (chocolates)
+chocolatesdf(chocolates)
+   
+students ={"steve": 32, "Lia":28, "vin":45, "katie":38}
+studentsdata = [students]
+print(studentsdata)
+
+studentsdf = pandas.dataFrame(studentdata, index = ["age"])
+
+
+studentlist =[[ "steve", 32,"male"], ["lia", 28,"female"], ["vin",45, "male"], ["katie", 38, "female"]]
+studentlistdf = pandas.DataFrame(studentlist,columns=["name","age", "gender"],index= ["1","2","3","4"])
+print(studentlistdf)
+
+print(studentinfo)
+print(studentgender)
+
+studentdf1 = [studentinfo,studentgender]
+print(studentdf1)
+
+studentdf2 = pandas.DataFrame(studentdf1,index=["age","gender"])
+print(studentdf2)
+
+
+
+
+
+
+
+
+
+
+
+
+
